e8c9e80142557bd502501f01c669b98378756744
karthik.c <karthik.c@lnttechservices.com>
2016-04-20 08:01:45 +0530

    eee

--- IstanbulTest/adele.js
+++ IstanbulTest/adele.js
@@ -1,22 +1,20 @@
+function adele() {
+    this.song_1 = 'Rolling In the Deep';
 
-
-function adele (){
-  this.song_1 = 'Rolling In the Deep';
-
-  this.getSongCollection = function(){
-    var collection = [
-      'Rolling In the Deep',
-      'Someone Like You',
-      'Set Fire to the Rain',
-      'Skyfall',
-      'Turning Tables'
-    ];
-    return collection
-  }
+    this.getSongCollection = function() {
+        var collection = [
+            'Rolling In the Deep',
+            'Someone Like You',
+            'Set Fire to the Rain',
+            'Skyfall - This is the end',
+            'Turning Tables'
+        ];
+        return collection
+    }
 }
 
 var adele_songs = new adele();
-console.log('\nadele_songs :',adele_songs);
-console.log('\nadele song 1 :',adele_songs.song_1);
-console.log('\nadele song collection:',adele_songs.getSongCollection());
-module.exports = adele ;
+console.log('\nadele_songs :', adele_songs);
+console.log('\nadele song 1 :', adele_songs.song_1);
+console.log('\nadele song collection:', adele_songs.getSongCollection());
+module.exports = adele;
